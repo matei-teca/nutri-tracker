1a8ea301c7c7753b866cdd9b4f1f7c76f38c4859
Valentin1289 <valibaiacof1289@gmail.com>
2023-03-13 21:14:20 +0200

    Chart for nutrients

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import DoughnutChart from "./components/DoughnutChart";
 
 function App() {
-
   return (
     <div className="App">
-      
+      <DoughnutChart />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
