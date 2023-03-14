9823543ba6f45cdd012a9575c0d90ee906dcc6ba
Valentin1289 <valibaiacof1289@gmail.com>
2023-03-14 22:05:11 +0200

    basic style

--- client/src/components/DoughnutChart.jsx
+++ client/src/components/DoughnutChart.jsx
@@ -3,23 +3,23 @@ import { VictoryPie } from "victory";
 export default function DoughnutChart(props) {
   let nutriments = props.nutriments;
   const data = [
-    { x: "sugars", y: nutriments.sugars },
-    { x: "fibers", y: nutriments.fiber },
-    { x: "carbo", y: nutriments.carbohydrates },
-    { x: "fat", y: nutriments.fat },
-    { x: "protein", y: nutriments.proteins },
+    { x: " ", y: nutriments.carbohydrates },
+    { x: " ", y: nutriments.fat },
+    { x: " ", y: nutriments.proteins },
+    { x: " ", y: nutriments.fiber },
+    { x: " ", y: nutriments.sugars },
   ];
-
+console.log(1)
   return (
     <div style={{ width: "400px", height: "400px" }}>
       <VictoryPie
         data={data}
         innerRadius={80}
         padding={90}
-        colorScale={["#f74bd7", "#019300", "#ebe80e", "#ae6b00", "#003aed"]}
+        colorScale={["rgb(255, 151, 48)", "rgb(94, 87, 81)", "#003aed", "#019300", "#f74bd7"]}
         animate={{ duration: 2000 }}
         responsive={true}
-        cornerRadius={({ datum }) => datum.y * 0.2}
+        padAngle={({ datum }) => datum.y*0.1}
       />
     </div>
   );
--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -7,13 +7,36 @@ export default function ProductDetails() {
   return (
     product &&
     (product !== "Product Not Found" ? (
-      <div className="product-details">
-        <h1>{product.name}</h1>
-        <DoughnutChart nutriments={product.nutriments} />
+      <div className="product-chart">
+        <div>
+          <h1>{product.name}</h1>
+          <DoughnutChart nutriments={product.nutriments} />
+        </div>
+        <div className="product-table">
+          <ul>
+            <li style={{ color: "rgb(255, 151, 48)" }}>Carbohydrates</li>
+            <li style={{ color: "rgb(94, 87, 81)" }}>Fat</li>
+            <li style={{ color: "#003aed" }}>Proteins</li>
+            <li style={{ color: "#019300" }}>Fiber</li>
+            <li style={{ color: "#f74bd7" }}>Sugars</li>
+          </ul>
+        </div>
+        <div className="product-table-values">
+          <ul>
+            <li style={{ color: "rgb(255, 151, 48)" }}>
+              {product.nutriments.carbohydrates} g
+            </li>
+            <li style={{ color: "rgb(94, 87, 81)" }}>{product.nutriments.fat} g</li>
+            <li style={{ color: "#003aed" }}>
+              {product.nutriments.proteins} g
+            </li>
+            <li style={{ color: "#019300" }}>{product.nutriments.fiber} g</li>
+            <li style={{ color: "#f74bd7" }}>{product.nutriments.sugars} g</li>
+          </ul>
+        </div>
       </div>
     ) : (
-      <div>
-      </div>
+      <div></div>
     ))
   );
 }
